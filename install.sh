e0c05a4773f46e1c4496767fc445aa41d57217d3
Matheus Bach <35426162+matheusbach@users.noreply.github.com>
2019-02-07 13:23:14 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-read -p "Do you want me to install Group Butler Bot? (Y/N): "
-
-if [ "$REPLY" != "Y" ]; then
-    echo "Exiting..."
-else
     echo -e "\e[1;36mUpdating packages\e[0m"
     sudo apt-get update -y
 
@@ -26,17 +21,4 @@ else
     for rock in $rocks; do
         sudo luarocks install $rock
     done
-        
-    branch="master"
-    read -p "Do you want to use the beta branch? (Y/N): "
-    
-    if [ "$REPLY" == "Y" ]; then
-        branch="beta"
-    fi
-    
-    echo -e "\e[1;36mFetching latest Group Butler source code (branch: $branch)\e[0m"
-    git clone -b $branch https://github.com/RememberTheAir/GroupButler.git
-    
-    echo -e "\e[1;32mGroup Butler successfully installed! Change values in config file and run ./launch.sh\e[0m"
-    echo " "
-fi
+       
