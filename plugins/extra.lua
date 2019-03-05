542fbdb05433edea062ca3811f17d3570afec9ff
Matheus Bach <math.bach.com@gmail.com>
2019-03-04 21:11:51 -0300

    Adicionado antispam no /extra

--- plugins/extra.lua
+++ plugins/extra.lua
@@ -71,43 +71,54 @@ function plugin.onTextMessage(msg, blocks)
 	        local out = _("The command '%s' does not exist!"):format(blocks[2])
 	        api.sendReply(msg, out)
 	    end
-    else
+	else
     	local hash = 'chat:'..msg.chat.id..':extra'
     	local text = db:hget(hash, blocks[1])
-        if not text then return true end --continue to match plugins
-        local file_id = text:match('^###.+###:(.*)')
-        local special_method = text:match('^###file_id!(.*)###') --photo, voices, video need their method to be sent by file_id
-    	local link_preview = text:find('telegra%.ph/') ~= nil
-        if is_locked(msg.chat.id) and not msg.from.mod then --send it in private
-        	if not file_id then
-        		local reply_markup, clean_text = u.reply_markup_from_text(text)
-            	api.sendMessage(msg.from.id, clean_text:replaceholders(msg.reply or msg), true, reply_markup, nil, link_preview)
-            else
-            	if special_method then
-            		api.sendMediaId(msg.from.id, file_id, special_method) --photo, voices, video need their method to be sent by file_id
+		if not text then return true end --continue to match plugins
+				
+		-- antispam inicio --
+		local key_comando = 'chat:'..msg.chat.id..':'..text
+		local last_user = db:get(key_comando)
+		if last_user then
+		else
+			local nome = u.getname_final(msg.from)
+			db:setex(key_comando, 10800, nome)
+		-- antispam fim --
+
+        	local file_id = text:match('^###.+###:(.*)')
+        	local special_method = text:match('^###file_id!(.*)###') --photo, voices, video need their method to be sent by file_id
+    		local link_preview = text:find('telegra%.ph/') ~= nil
+        	if is_locked(msg.chat.id) and not msg.from.mod then --send it in private
+        		if not file_id then
+        			local reply_markup, clean_text = u.reply_markup_from_text(text)
+            		api.sendMessage(msg.from.id, clean_text:replaceholders(msg.reply or msg), true, reply_markup, nil, link_preview)
             	else
-            		api.sendDocumentId(msg.from.id, file_id)
+            		if special_method then
+            			api.sendMediaId(msg.from.id, file_id, special_method) --photo, voices, video need their method to be sent by file_id
+            		else
+            			api.sendDocumentId(msg.from.id, file_id)
+            		end
             	end
-            end
-        else
-        	local msg_to_reply
-        	if msg.reply then
-        		msg_to_reply = msg.reply.message_id
         	else
-        		msg_to_reply = msg.message_id
-        	end
-        	if file_id then
-        		if special_method then
-        			api.sendMediaId(msg.chat.id, file_id, special_method, msg_to_reply) --photo, voices, video need their method to be sent by file_id
+        		local msg_to_reply
+        		if msg.reply then
+        			msg_to_reply = msg.reply.message_id
         		else
-        			api.sendDocumentId(msg.chat.id, file_id, msg_to_reply)
+        			msg_to_reply = msg.message_id
+	        	end
+    	    	if file_id then
+        			if special_method then
+        				api.sendMediaId(msg.chat.id, file_id, special_method, msg_to_reply) --photo, voices, video need their method to be sent by file_id
+        			else
+        				api.sendDocumentId(msg.chat.id, file_id, msg_to_reply)
+        			end
+    			else
+    				local reply_markup, clean_text = u.reply_markup_from_text(text)
+        			api.sendMessage(msg.chat.id, clean_text:replaceholders(msg.reply or msg), true, reply_markup, msg_to_reply, link_preview) --if the mod replies to an user, the bot will reply to the user too
         		end
-    		else
-    			local reply_markup, clean_text = u.reply_markup_from_text(text)
-        		api.sendMessage(msg.chat.id, clean_text:replaceholders(msg.reply or msg), true, reply_markup, msg_to_reply, link_preview) --if the mod replies to an user, the bot will reply to the user too
         	end
-        end
-    end
+		end
+	end
 end
 
 plugin.triggers = {
