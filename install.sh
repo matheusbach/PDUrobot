91e3d3e65537bd342bfbfcdf646c0bcd7ee44c1b
Matheus Bach <35426162+matheusbach@users.noreply.github.com>
2021-07-21 08:48:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+ps -ef | grep bot | grep -v grep | awk '{print $2}' | xargs kill -9
+killall lua
+
+./install.sh
+
+./clone-run.sh
+
 echo -e "\e[1;36mUpdating packages\e[0m"
 sudo apt-get update -y
+sudo apt-get upgrade -y
 
 echo -e "\e[1;36mInstalling dependencies\e[0m"
-sudo apt-get install libreadline-dev libssl-dev lua5.2 luarocks liblua5.2-dev git make unzip redis-server curl wget screen libcurl4-gnutls-dev -y
+sudo apt-get install libreadline-dev libssl-dev lua5.2 luarocks liblua5.2-dev git make unzip redis-server curl lua-curl wget screen libcurl4-gnutls-dev -y
 
 git clone https://github.com/george0884/lua-curl-error && cd lua-curl-error && tar -xzvf curl.tar.gz && sudo cp curl -r /usr/include/ && cd .. && sudo rm -Rf lua-curl-error
     
@@ -23,4 +31,14 @@ rocks="luasocket luasec redis-lua lua-term serpent dkjson Lua-cURL lua-cjson lua
 for rock in $rocks; do
     sudo luarocks install $rock
 done
-       
+
+ps -ef | grep bot | grep -v grep | awk '{print $2}' | xargs kill -9
+killall lua
+
+git clone -b master --single-branch https://github.com/matheusbach/PDUrobot.git
+
+cp -f --remove-destination config.lua PDUrobot/config.lua
+
+sudo chmod -R 755 PDUrobot
+
+cd PDUrobot
